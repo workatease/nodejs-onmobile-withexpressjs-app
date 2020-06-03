b883bea04e65d6b45e26586afec8382cc5a19aab
Pyll Gomez <gomezatwork@gmail.com>
2020-06-02 21:39:55 -0400

    to write a sample file to check rn_bridge.app.datadir()

--- nodejs-assets/nodejs-project/main.js
+++ nodejs-assets/nodejs-project/main.js
@@ -6,14 +6,14 @@ var rn_bridge = require('rn-bridge');
 // Echo every message received from react-native.
 rn_bridge.channel.on('message', (msg) => {
   rn_bridge.channel.send(msg);
-} );
+});
 
 // Inform react-native node is initialized.
 rn_bridge.channel.send("Node was initialized.");
 try {
-  require('./server')(function() {
+  require('./server')(function () {
     rn_bridge.channel.send('Dev app listening on port 9000!');
-  });
+  }, rn_bridge.app.datadir());
 } catch (error) {
   console.log(error);
 }
--- nodejs-assets/nodejs-project/server.js
+++ nodejs-assets/nodejs-project/server.js
@@ -1,5 +1,11 @@
 
-module.exports = function setupServer(callback) {
+module.exports = function setupServer(callback, dataDir) {
+
+    var fs = require('fs');
+    fs.writeFile(dataDir + '/helloworld.txt', 'Hello World!', function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+    });
     const express = require('express');
     const app = express();
     var PouchDB = require('pouchdb');
@@ -9,10 +15,10 @@ module.exports = function setupServer(callback) {
     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     console.log('Sent list of items');
 
-    const db = new PouchDB('test');
+    const db = new PouchDB(dataDir+'/test');
     const remoteDb = PouchDB('https://f95f5018-4c30-469c-a0bd-2f1c1d710d47-bluemix:492de14f6ab1ef4bf736b4bcbc7a33c72159a28b7aaeb8f602885c7aa7a92d45@f95f5018-4c30-469c-a0bd-2f1c1d710d47-bluemix.cloudantnosqldb.appdomain.cloud/test')
-    
-    
+
+
     db.sync(remoteDb, {
         live: true,
         retry: true
